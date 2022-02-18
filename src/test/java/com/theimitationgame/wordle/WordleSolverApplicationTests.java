34a112fe0d6a14e5db01993030d2f63e3d66c43e
Cristian Rosca <cristianrrosca@gmail.com>
2022-02-18 19:00:38 +0000

    initial commit

---
+++ src/test/java/com/theimitationgame/wordle/WordleSolverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.theimitationgame.wordle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordleSolverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
